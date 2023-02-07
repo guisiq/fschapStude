64959d8617d3239e9431cf07fb00c53cfb2fd86b
guilherme01 <lopesgui01@gmail.com>
2023-02-07 15:26:25 -0300

    condicionais

---
+++ testes/Program.fs
@@ -0,0 +1,21 @@
+﻿System.Console.Write "Type a value:"
+let str = System.Console.ReadLine()
+printfn "You typed %s" str;;
+
+//#%
+
+// let first = "32"
+// let numberVersion = System.Int32.Parse(first) 
+// printfn "Number %i" numberVersion // Output: Number 32
+
+//#%
+
+let first = "32"
+let numberVersion =  int first 
+printfn "Number %i" numberVersion;;
+
+//#%
+
+let no = 10
+let isDivisibleByTwo = no % 2 = 0
+printfn "Divisible by two %b" isDivisibleByTwo;;
