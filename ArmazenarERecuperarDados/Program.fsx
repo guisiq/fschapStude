a490aaef91d588391dbcf412383b98c181820094
guilherme01 <lopesgui01@gmail.com>
2023-02-07 15:08:07 -0300

    muldando o caminho

--- ArmazenarERecuperarDados/Program.fsx
+++ ArmazenarERecuperarDados/Program.fsx
@@ -1,6 +1,6 @@
 ﻿System.Console.Write "Type a value:"
 let str = System.Console.ReadLine()
-printfn "You typed %s" str
+printfn "You typed %s" str;;
 
 //#%
 
@@ -12,10 +12,10 @@ printfn "You typed %s" str
 
 let first = "32"
 let numberVersion =  int first 
-printfn "Number %i" numberVersion
+printfn "Number %i" numberVersion;;
 
 //#%
 
 let no = 10
 let isDivisibleByTwo = no % 2 = 0
-printfn "Divisible by two %b" isDivisibleByTwo
+printfn "Divisible by two %b" isDivisibleByTwo;;
