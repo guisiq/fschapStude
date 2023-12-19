c42f46f35bd633fc438994efd2137eba21d0fd91
guilhermehse <lopesgui01@gmail.com>
2023-12-19 20:50:01 +0000

    refatorando metodo

--- codeWars/TreeNode/treeNode/Program.cs
+++ codeWars/TreeNode/treeNode/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 Func<int, IEnumerable<int>> rootCombnaitions = x => {
     if (x == 0||x < 10)
     {
@@ -10,27 +9,28 @@ Func<int, IEnumerable<int>> rootCombnaitions = x => {
          return new List<int> { x.ToString().Select(y => int.Parse(y.ToString())).Sum() };
     }
 };
+
 var root = new Dictionary<int,TreeNode<int>>();
-var rootSum = TreeNode<int>.buildTree(rootCombnaitions,value: 9999,Depth: 3,root: root);
+
+var rootSum = TreeNodehelper.BuildTree(rootCombnaitions,value: 9999,Depth: 3,root: root);
 //todadas as combinacoes de numeros de 2 digitos com //[5,6,7,8,9] podendo repitir 
 List<string> GenerateCombinations(List<int> numbers, int digitCount)
-{
-    if (digitCount == 1)
-    {
-        return numbers.Select(n => n.ToString()).ToList();
-    }
-    else
-    {
-        var result = new List<string>();
-        var smallerCombinations = GenerateCombinations(numbers, digitCount - 1);
-
-        foreach (var number in numbers)
+{   
+    IQueryable<String> aux(IQueryable<int> numbers2, int digitCount2){
+        if (digitCount2 == 1)
         {
-            result.AddRange(smallerCombinations.Select(c => number.ToString() + c));
+            return numbers2.Select(n => n.ToString());
         }
-
-        return result;
+        else
+        {
+            return aux(numbers2, digitCount2 - 1).SelectMany(x => numbers2, (x, y) => x + y.ToString());
+        }
     }
+    // recupera a expressao linq para que possa ser executada posteriormente
+    // var ex = aux(numbers.AsQueryable(), digitCount).Expression;
+    // var CanReduce = ex.CanReduce;
+    // var x2 = ex.ReduceExtensions;
+    return aux(numbers.AsQueryable(), digitCount).ToList();
 }
 var combinations2 = GenerateCombinations(new List<int> { 5, 6, 7, 8, 9 }, 2);
 var combinations3e2 = GenerateCombinations(new List<int> { 5, 6, 7, 8, 9 }, 3)
@@ -41,12 +41,12 @@ combinations3e2.ForEach(x =>
 {
     //[5,6,7,8,9] 
     Console.WriteLine(x);
-    rootSum = TreeNode<int>.buildTree(rootCombnaitions,value: x ,Depth: 3,root: root);
+    rootSum = TreeNodehelper.BuildTree(rootCombnaitions,value: x ,Depth: 3,root: root);
 });
 //força a execulcao tardia dos metodos linq para que o plantUML possa ser gerado
 
-var plantUmlOutput = TreeNode<int>.PrintTreeforplantUML(rootSum);
+var plantUmlOutput = rootSum.PrintTreeforplantUML();
 Console.WriteLine(plantUmlOutput);
-TreeNode<int>.InvertTree(rootSum).ForEach(x => Console.WriteLine(TreeNode<int>.PrintTreeforplantUML(rootSum)));
+rootSum.InvertTree().GetEnumerator().Current.PrintTreeforplantUML();
 Console.WriteLine(plantUmlOutput);
 
